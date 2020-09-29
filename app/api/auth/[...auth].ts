6a4ae72ba362b2d3716a9a301dc081dab057c867
Gimel Dick <gimeldick@gmail.com>
2020-09-29 00:38:29 -0400

    Updated passport google callback

--- app/api/auth/[...auth].ts
+++ app/api/auth/[...auth].ts
@@ -11,7 +11,10 @@ export default passportAuth({
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/api/auth/google/callback",
+        callbackURL:
+          process.env.NODE_ENV === "production"
+            ? "https://webble.co/api/auth/google/callback"
+            : "http://localhost:3000/api/auth/google/callback",
       },
       async function (accessToken, refreshToken, profile, done) {
         const email = profile.email ?? profile.emails[0]?.value

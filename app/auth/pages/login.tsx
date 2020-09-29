27190f6a6f20f7a377454559efec38dc081dcd57
Gimel Dick <gimeldick@gmail.com>
2020-09-29 00:24:46 -0400

    Attempt to fix same-site prod err

--- app/auth/pages/login.tsx
+++ app/auth/pages/login.tsx
@@ -2,21 +2,21 @@ import React from "react"
 import { useRouter, BlitzPage, GetServerSideProps, Router } from "blitz"
 import Layout from "app/layouts/Layout"
 import { LoginForm } from "app/auth/components/LoginForm"
-import { getSessionContext } from "@blitzjs/server"
+// import { getSessionContext } from "@blitzjs/server"
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSessionContext(context.req, context.res)
-  if (session.userId) {
-    if (typeof window === "undefined") {
-      context.res.writeHead(302, { Location: "/" })
-      context.res.end()
-    }
-    Router.push("/")
-  }
-  return {
-    props: {},
-  }
-}
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await getSessionContext(context.req, context.res)
+//   if (session.userId) {
+//     if (typeof window === "undefined") {
+//       context.res.writeHead(302, { Location: "/" })
+//       context.res.end()
+//     }
+//     Router.push("/")
+//   }
+//   return {
+//     props: {},
+//   }
+// }
 
 const LoginPage: BlitzPage = () => {
   const router = useRouter()

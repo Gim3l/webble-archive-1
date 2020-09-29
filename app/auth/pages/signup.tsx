4f56e01aee9e175f5ba1e189414b737bb1aa2066
Gimel Dick <gimeldick@gmail.com>
2020-09-29 00:43:09 -0400

    Disabled getServerSideProps to fix prod issue (for signup)

--- app/auth/pages/signup.tsx
+++ app/auth/pages/signup.tsx
@@ -4,19 +4,19 @@ import Layout from "app/layouts/Layout"
 import { SignupForm } from "app/auth/components/SignupForm"
 import { getSessionContext } from "@blitzjs/server"
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSessionContext(context.req, context.res)
-  if (session.userId) {
-    if (typeof window === "undefined") {
-      context.res.writeHead(302, { Location: "/" })
-      context.res.end()
-    }
-    Router.push("/")
-  }
-  return {
-    props: {},
-  }
-}
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await getSessionContext(context.req, context.res)
+//   if (session.userId) {
+//     if (typeof window === "undefined") {
+//       context.res.writeHead(302, { Location: "/" })
+//       context.res.end()
+//     }
+//     Router.push("/")
+//   }
+//   return {
+//     props: {},
+//   }
+// }
 
 const SignupPage: BlitzPage = () => {
   const router = useRouter()

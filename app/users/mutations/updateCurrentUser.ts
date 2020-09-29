e7a9bd8bdea17a75233d095b8b81d5739878549b
Gimel Dick <gimeldick@gmail.com>
2020-09-29 01:11:41 -0400

    Unintentional authorization

--- app/users/mutations/updateCurrentUser.ts
+++ app/users/mutations/updateCurrentUser.ts
@@ -9,7 +9,7 @@ export default async function updateCurrentUser(
   { data }: UserUpdateInput,
   ctx: { session?: SessionContext } = {}
 ) {
-  ctx.session!.authorize()
+  if (!ctx.session?.userId) return null
 
   const user = await db.user.update({ data, where: { id: ctx.session!.userId } })
 

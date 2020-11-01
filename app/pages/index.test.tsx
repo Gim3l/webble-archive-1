49db97f397a0f1b35da4c7a4848590fe4952814f
Gimel Dick <gimeldick@gmail.com>
2020-11-01 15:20:03 -0400

    Updated homepage

--- app/pages/index.test.tsx
+++ app/pages/index.test.tsx
@@ -1,36 +1,36 @@
-import React from "react"
-import { render } from "test/utils"
+// import React from "react"
+// import { render } from "test/utils"
 
-import Home from "./index"
-import { useCurrentUser } from "app/hooks/useCurrentUser"
-import { useCurrentProfile } from "app/hooks/useCurrentProfile"
+// import Home from "./index"
+// import { useCurrentUser } from "app/hooks/useCurrentUser"
+// import { useCurrentProfile } from "app/hooks/useCurrentProfile"
 
-jest.mock("app/hooks/useCurrentUser")
-const mockUseCurrentUser = useCurrentUser as jest.MockedFunction<typeof useCurrentUser>
-const mockUseCurrentProfile = useCurrentProfile as jest.MockedFunction<typeof useCurrentProfile>
+// jest.mock("app/hooks/useCurrentUser")
+// // const mockUseCurrentUser = useCurrentUser as jest.MockedFunction<typeof useCurrentUser>
+// // const mockUseCurrentProfile = useCurrentProfile as jest.MockedFunction<typeof useCurrentProfile>
 
-test.skip("renders blitz documentation link", () => {
-  // This is an example of how to ensure a specific item is in the document
-  // But it's disabled by default (by test.skip) so the test doesn't fail
-  // when you remove the the default content from the page
+// // test.skip("renders blitz documentation link", () => {
+// //   // This is an example of how to ensure a specific item is in the document
+// //   // But it's disabled by default (by test.skip) so the test doesn't fail
+// //   // when you remove the the default content from the page
 
-  // This is an example on how to mock api hooks when testing
-  mockUseCurrentUser.mockReturnValue({
-    id: 1,
-    firstName: "John",
-    lastName: "Doe",
-    email: "user@email.com",
-    role: "user",
-  })
+// //   // This is an example on how to mock api hooks when testing
+// //   mockUseCurrentUser.mockReturnValue({
+// //     id: 1,
+// //     firstName: "John",
+// //     lastName: "Doe",
+// //     email: "user@email.com",
+// //     role: "user",
+// //   })
 
-  mockUseCurrentProfile.mockReturnValue({
-    id: 1,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    userId: 2,
-  })
+// //   mockUseCurrentProfile.mockReturnValue({
+// //     id: 1,
+// //     createdAt: new Date(),
+// //     updatedAt: new Date(),
+// //     userId: 2,
+// //   })
 
-  const { getByText } = render(<Home />)
-  const linkElement = getByText(/Documentation/i)
-  expect(linkElement).toBeInTheDocument()
-})
+// //   const { getByText } = render(<Home />)
+// //   // const linkElement = getByText(/Documentation/i)
+// //   // expect(linkElement).toBeInTheDocument()
+// // })

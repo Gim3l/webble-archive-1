cb531f318926c914c96329620490f5e9ec6baf2b
Gimel Dick <gimeldick@gmail.com>
2020-11-05 02:02:43 -0400

    Updated dist for pwa

--- blitz.config.js
+++ blitz.config.js
@@ -5,7 +5,7 @@ module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV !== "production",
     register: true,
-    dest: "public",
+    dest: "../../../public",
   },
   middleware: [
     sessionMiddleware({

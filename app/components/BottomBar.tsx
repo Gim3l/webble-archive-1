a1fbabb48bd9f904662d2d14b47a581ff2a712f0
Gimel Dick <gimeldick@gmail.com>
2020-11-05 19:24:15 -0400

    Update manifest reference

--- app/components/BottomBar.tsx
+++ app/components/BottomBar.tsx
@@ -1,5 +1,5 @@
 import { Link, useParam } from "blitz"
-import { Box, Columns, Hide, Text, useBreakpoint } from "bumbag"
+import { Box, Columns, Hide, Icon, Text, useBreakpoint } from "bumbag"
 import React from "react"
 
 function BottomBar() {
@@ -21,18 +21,25 @@ function BottomBar() {
           alignItems="center"
         >
           <Link href={"/classroom/" + classroomId}>
+            <Icon aria-label="Dashboard" icon="solid-house-user" marginRight="minor-3" />{" "}
             <Text fontSize="100">Dashboard</Text>
           </Link>
           <Link href={"/classroom/" + classroomId + "/resources"}>
+            <Icon aria-label="Resources" icon="solid-folder" marginRight="minor-3" />
+
             <Text fontSize="100">Resources</Text>
           </Link>
           <Link href={"/classroom/" + classroomId + "/assignments"}>
+            <Icon aria-label="Assignments" icon="solid-book" marginRight="minor-3" />
+
             <Text fontSize="100">Assignments</Text>
           </Link>
           <Link href="#">
+            <Icon aria-label="Schedule" icon="solid-calendar-week" marginRight="minor-3" />
             <Text fontSize="100">Schedule</Text>
           </Link>
           <Link href="#">
+            <Icon aria-label="Grades" icon="solid-percentage" marginRight="minor-3" />
             <Text fontSize="100">Grades</Text>
           </Link>
         </Box>
